3e9ec67dcf581179239eb76805e6dc9370850786
BakauDesign <hi.bakaudesign@gmail.com>
2025-09-19 06:51:18 +0700

    fix: add default and permission in addMenuLink

--- src/plugins/kirim-pesan-whatsapp/admin/src/index.ts
+++ src/plugins/kirim-pesan-whatsapp/admin/src/index.ts
@@ -17,8 +17,11 @@ export default {
       Component: async () => {
         const { App } = await import('./pages/App');
 
-        return App;
+        return {
+            default: App
+        };
       },
+      permissions: [],
     });
 
     app.registerPlugin({
